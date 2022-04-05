ec7836dbe872dcf9b91fc765ad4501f02539fbe0
Janice Chan <janice.chan1288@gmail.com>
2022-04-05 17:36:08 -0400

    commit tree be30f2ea03e6d7852e88143d75d0eabba2a0499a

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 function isPalindrome(word) {
   // Write your algorithm here
+let string = word.split('').reverse().join('');
+if (word === string) {
+  return true
+} else {
+  return false
 }
-
+}
+//isPalindrome("racecar")
 /* 
   Add your pseudocode here
+  reverse the string
 */
 
 /*
   Add written explanation of your solution here
+split() method splits the string into an array
+reverse() reverses an array
+join() joins all elemes of an array into a string
 */
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
@@ -16,7 +27,7 @@ if (require.main === module) {
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+  console.log("mother");
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
